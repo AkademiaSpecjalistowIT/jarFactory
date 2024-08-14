a22937592bd87cb83a3620a14a0f84c3f16ec5c8
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-08-14 20:06:47 +0200

    init

---
+++ src/main/java/pl/akademiaspecjalistowit/jarfactory/JarFactoryApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.jarfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akademiaspecjalistowit/jarfactory/JarFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.jarfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
