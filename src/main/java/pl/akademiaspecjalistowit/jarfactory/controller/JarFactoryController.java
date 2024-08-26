bde802f6b04e8638bc3ceb59d0960418b587ad55
Sergi <142085017+sginko@users.noreply.github.com>
2024-08-26 08:33:07 +0200

    014 Dodanie do Open-API endpointu do modyfikacji złożonego zamówienia. (#7)
    * add
    
    * add
    
    * add

--- src/main/java/pl/akademiaspecjalistowit/jarfactory/controller/JarFactoryController.java
+++ src/main/java/pl/akademiaspecjalistowit/jarfactory/controller/JarFactoryController.java
@@ -2,6 +2,7 @@ package pl.akademiaspecjalistowit.jarfactory.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.akademiaspecjalistowit.jarfactory.model.JarOrderRequestDto;
 import pl.akademiaspecjalistowit.jarfactory.service.JarOrderService;
@@ -14,9 +15,16 @@ import java.util.jar.JarException;
 @RequestMapping("/api/v1/jars")
 public class JarFactoryController {
     private final JarOrderService jarOrderService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/order")
     public UUID createNewJarsOrder(@RequestBody JarOrderRequestDto jarOrderRequestDto) throws JarException {
         return jarOrderService.addOrder(jarOrderRequestDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/order")
+    public ResponseEntity<String> updateJarOrder(@RequestBody JarOrderRequestDto jarOrderRequestDto) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("NOT_IMPLEMENTED");
     }
 }
