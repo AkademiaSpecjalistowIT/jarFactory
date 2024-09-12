53a306074d5f26539f1178432179820654fe0c20
sginko <142085017+sginko@users.noreply.github.com>
2024-09-12 18:05:17 +0200

    add

--- src/main/java/pl/akademiaspecjalistowit/jarfactory/controller/JarsController.java
+++ src/main/java/pl/akademiaspecjalistowit/jarfactory/controller/JarsController.java
@@ -1,22 +1,20 @@
 package pl.akademiaspecjalistowit.jarfactory.controller;
 
+import com.github.fge.jsonpatch.JsonPatch;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.akademiaspecjalistowit.jarfactory.model.JarOrderRequestDto;
 import pl.akademiaspecjalistowit.jarfactory.model.OrderResponse;
-import pl.akademiaspecjalistowit.jarfactory.model.PatchRequestInner;
 import pl.akademiaspecjalistowit.jarfactory.service.JarOrderService;
 
-import java.util.List;
 import java.util.UUID;
+
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("api/v1")
-public class JarsController implements JarsApi {
+public class JarsController implements ApiApi {
     private final JarOrderService jarOrderService;
 
     @Override
@@ -26,8 +24,9 @@ public class JarsController implements JarsApi {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @Override
-    public ResponseEntity<Void> patchJarOrder(UUID id, List<PatchRequestInner> patchRequestInner) {
-        return JarsApi.super.patchJarOrder(id, patchRequestInner);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/order/{id}")
+    public void updateJarOrder(@PathVariable("id") UUID orderId, @RequestBody JsonPatch patch) {
+        jarOrderService.updateOrder(orderId, patch);
     }
 }
